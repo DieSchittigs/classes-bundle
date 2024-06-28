05022713829bc330aaad3f42dab5e8dfc9c15c76
Martin Schittig <martin@dieschittigs.de>
2024-06-28 09:21:20 +0200

    fix bug for content element article

--- src/Resources/contao/classes/HelperClass.php
+++ src/Resources/contao/classes/HelperClass.php
@@ -41,7 +41,7 @@ class HelperClass extends Frontend
     //public function addClassesToElement($objRow, $strBuffer, $objElement)
     public function addClassesToElement(ContentModel $contentModel, string $buffer, $element)
     {
-        $cssId = unserialize($contentModel->cssID)[1];
+        $cssId = (is_array(unserialize($contentModel->cssID))) ? unserialize($contentModel->cssID)[1] : '';
         $classes = 'content-' . $contentModel->type . ((empty($cssId)) ? '' : ' ' . $cssId);
 
         if (!is_array($arrCustom = unserialize($contentModel->customClass))) return $buffer;
